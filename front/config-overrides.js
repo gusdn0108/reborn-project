8695344d226a13014b3a39e016adea9173314652
jongs <jongnam0309@gmail.com>
2022-06-03 10:44:15 +0900

    test1

---
+++ front/config-overrides.js
@@ -0,0 +1,11 @@
+const { override, addWebpackAlias } = require('customize-cra')
+
+module.exports = override(
+    config => {
+        config.optimization = {
+            ...config.optimization,
+            minimizer: config.optimization.minimize[0]
+        }
+        return config
+    }
+)
